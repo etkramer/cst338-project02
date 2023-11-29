169477877bb28e849fc49227d322a183f69f25fe
Elijah <elitkramer@gmail.com>
2023-11-29 12:51:50 -0800

    Add 'Go back' button to cart

--- app/src/main/java/com/etkramer/project02/CartActivity.kt
+++ app/src/main/java/com/etkramer/project02/CartActivity.kt
@@ -5,19 +5,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.etkramer.project02.databinding.ActivityCartBinding
 import com.etkramer.project02.databinding.ActivityLoginBinding
 import com.etkramer.project02.db.AppDatabase
 
 class CartActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
+    private lateinit var binding: ActivityCartBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityCartBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val db = AppDatabase.getInstance(this)
+        binding.backButton.setOnClickListener {
+            finish()
+        }
     }
 
     companion object {
